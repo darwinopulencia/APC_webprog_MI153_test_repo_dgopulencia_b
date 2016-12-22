3dd7b257ed5d9c1e00780cf5a9095d4ab84109f9
darwinopulencia <dgopulencia@gmail.com>
2016-12-22 13:39:13 +0800

    Added

---
+++ Exercise6/application/views/templates/header.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>CodeIgniter Tutorial</title>
+	</head>
+	<body>
+		<h1>My Website</h1>
+		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+		<center>
+<CENTER>
+		<CENTER>
+
+		<br>
+		<br>
+		<table align="center">
+			<tr>
+				<td><img width ="231" height = "231" src="<?php echo base_url();?>/pics/nikee.png" border="3"/><CENTER></td>
+			</tr>
